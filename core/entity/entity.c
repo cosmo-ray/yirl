d50e732b2e8e907ec052a26ad1cfe767e161f71e
matthias <uso.cosmo.ray@gmail.com>
2015-10-29 09:25:10 +0100

    entity: style fix
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- core/entity/entity.c
+++ core/entity/entity.c
@@ -27,7 +27,11 @@ inline static void yeDestroyInternal(Entity *entity);
 /**
  * Here some macros to mutualise the code of entity
  */
-#define RETURN_ERROR_BAD_TYPE(function, entity, returnValue) DPRINT_INFO("%s: bad entity, this entity is of type %s\n", (function), yeTypeToString(yeType(entity))); return (returnValue)
+#define RETURN_ERROR_BAD_TYPE(function, entity, returnValue) do {	\
+  DPRINT_INFO("%s: bad entity, this entity is of type %s\n",		\
+	      (function), yeTypeToString(yeType(entity)));		\
+  return (returnValue);							\
+  } while (0)
 
   /* macro for perf purpose */
 #define YE_INCR_REF(entity) do {		\
