8d0af70d086555abeac9eb16c6e41bab858f68c1
Adrien <adrien.ph@gmail.com>
2016-01-04 16:53:29 +0100

    core: Add sound manager
    Signed-off-by: Adrien <adrien.ph@gmail.com>

---
+++ include/core/sound-libvlc.h
@@ -0,0 +1,69 @@
+/*
+* Copyright 2015 Kara adrien
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _SOUND_LIB_VLC_H
+#define _SOUND_LIB_VLC_H
+
+/**
+ * libvlc manager
+ *
+ * \same of sound_manager
+ */
+int libvlc(int nameId, uint32_t flags, int soundLvl, const char *path);
+
+/**
+ * Play media file
+ *
+ * \param nameId the ID of media instance name
+ * \param path is the media path
+ * \param loop, play if zero, play loop if non-zero
+ * \return 0 upon success, -1 on error
+ */
+int libvlc_play(int nameId, const char *path, int loop);
+
+/**
+ * Set current media volume
+ *
+ * \param nameId the ID of media instance name
+ * \param soundLvl the volume in percents (0 = mute, 100 = 0dB)
+ * \return 0 if the volume was set, -1 if it was out of range
+ */
+int libvlc_soundLvl(int nameId, int soundLvl);
+
+/**
+ * Is the player able to play
+ *
+ * \param nameId the ID of media instance name
+ * \return 1 if media is playable, else 0
+ */
+int libvlc_status(int nameId);
+
+/**
+ * Pause or resume (no effect if there is no media)
+ * If media is already on pause resume, else ... pause
+ *
+ * \param nameId the ID of media instance name
+ */
+int libvlc_pause(int nameId);
+
+/**
+ * Stop (no effect if there is no media)
+ *
+ * \param nameId the ID of media instance name
+ */
+void libvlc_stop(int nameId);
+
+#endif
---
+++ include/core/sound.h
@@ -0,0 +1,97 @@
+/*
+* Copyright 2015 Kara adrien
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _SOUND_H
+#define _SOUND_H
+
+#define ARRAY_SIZE          64
+
+/* Flags on hexValue */
+#define PLAY_SOUND          0x00000001
+#define PLAY_LOOP_SOUND     0x00000002
+
+#define STOP_SOUND          0x00000004
+
+#define SOUND_LEVEL         0x00000010
+#define SOUND_STATUS        0x00000020
+#define SOUND_PAUSE         0x00000040
+
+/* Audio lib available */
+enum AudioLib { LIB_VLC, AUDIO_LIB_END };
+
+/**
+ * Select library you want
+ *
+ * \param libSelected name of lib you want
+ */
+void sound_init(enum AudioLib libSelected);
+
+/**
+ * Play, pause, stop, sound lvl, loop on media file
+ *
+ * \param name the media instance name
+ * \param flags for action you want
+ * \param soundLvl with SOUND_LEVEL flag define what sound level you want
+ * \param path is the media path
+ * \return 0 upon success, -1 on error
+ * \use SOUND_STATUS flags, return 1 if media is played, else 0
+ */
+int sound_manager(const char *name, uint32_t flags, int soundLvl, const char *path);
+
+/**
+ * Play sound, shortcut of sound_manager(name, PLAY_SOUND, 0, path);
+ *
+ * \param name the media instance name
+ * \param path is the media path
+ * \return 0 upon success, -1 on error
+ */
+int sound_play(const char *name, const char *path);
+
+/**
+ * Same of sound_play() in loop
+ */
+int sound_play_loop(const char *name, const char *path);
+
+/**
+ * Change sound level, shortcut of sound_manager(name, SOUND_LEVEL, 100, NULL);
+ *
+ * \param name the media instance name
+ * \param soundLvl with SOUND_LEVEL flag define what sound level you want
+ * \return 0 upon success, -1 on error
+ */
+int sound_level(const char *name, int soundLvl);
+
+/**
+ * Stop sound, shortcut of sound_manager(name, SOUND_STATUS, 0, NULL);
+ *
+ * \param name the media instance name
+ * \return 1 if media is playable, else 0
+ */
+int sound_status(const char *name);
+
+/**
+ * Stop sound, shortcut of sound_manager(name, STOP_SOUND, 0, NULL);
+ *
+ * \param name the media instance name
+ */
+void sound_stop(const char *name);
+
+/**
+ * Lib check width GLib (Unit testing)
+ */
+void yirl_sound_cpp(void);
+
+#endif
