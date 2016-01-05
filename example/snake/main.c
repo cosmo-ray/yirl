af43a065fc1d8256f6145bb99826e938e5ec5c4c
matthias <uso.cosmo.ray@gmail.com>
2016-01-06 00:23:47 +0100

    example: snake initial commit
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

---
+++ example/snake/main.c
@@ -0,0 +1,28 @@
+#include "game.h"
+
+static const char *gamePath = "./"; 
+
+static inline int die(int ret, GameConfig *cfg)
+{
+  ygCleanGameConfig(cfg);
+  ygEnd();
+  return ret;
+}
+
+#define TRY_OR_DIE(cmd, die)	 do {		\
+    if (cmd)					\
+      return die;				\
+  } while (0);
+
+int main(void)
+{
+  GameConfig cfg;
+
+  yuiDebugInit(); //Can not be init twice :)
+  TRY_OR_DIE(ygInitGameConfig(&cfg, gamePath, SDL2), -1);
+  TRY_OR_DIE(ygInit(&cfg), die(-1, &cfg));
+  TRY_OR_DIE(ygStartLoop(&cfg), die(-1, &cfg));
+  return die(0, &cfg);
+}
+
+#undef TRY_OR_DIE
