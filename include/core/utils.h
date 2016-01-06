4fb071471b94e4f9a4fdc7c82feebf502e751a0a
matthias <uso.cosmo.ray@gmail.com>
2016-01-06 09:34:38 +0100

    utils: add ywidStrEqual0
    This functions do the same as ywidStrEqual but check NULL.
    
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- include/core/utils.h
+++ include/core/utils.h
@@ -65,6 +65,16 @@ static inline int yuiStrEqual(const char *str1, const char *str2)
   return (str1[i] == str2[i]);
 }
 
+static inline int yuiStrEqual0(const char *str1, const char *str2)
+{
+  if (!str1 || !str2) {
+    if (str1 == str2)
+      return 1;
+    return 0;
+  }
+  return yuiStrEqual(str1, str2);
+}
+
 
 /**
  * example:
