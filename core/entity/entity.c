ea644ff9afed3c9f232f659bb56db36f0f215d85
matthias <uso.cosmo.ray@gmail.com>
2016-01-26 09:50:07 +0100

    entity: yeRemoveChild improve debug message
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- core/entity/entity.c
+++ core/entity/entity.c
@@ -434,7 +434,8 @@ int	yePushBack(Entity *entity, Entity *toPush, const char *name)
 Entity *yeRemoveChild(Entity *array, Entity *toRemove)
 {
   if (!checkType(array, YARRAY)) {
-    DPRINT_ERR("yeRemoveChild: bad entity\n");
+    DPRINT_ERR("yeRemoveChild: bad entity of type %s(should be array)\n",
+	       yeTypeToString(yeType(array)));
     return NULL;
   }
 
