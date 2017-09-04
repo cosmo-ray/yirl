598c71fc281d23d33807011ae9d819d182ea3d24
Matthias Gatto <uso.cosmo.ray@gmail.com>
2017-09-04 11:40:56 +0200

    tests: add text-screen test
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

---
+++ tests/textinput_mod.c
@@ -0,0 +1,41 @@
+/*
+**Copyright (C) 2017 Matthias Gatto
+**
+**This program is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Lesser General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+**
+**This program is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**GNU General Public License for more details.
+**
+**You should have received a copy of the GNU Lesser General Public License
+**along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "yirl/game.h"
+#include "tests.h"
+
+void testTextInputMod(void)
+{
+  yeInitMem();
+  GameConfig cfg;
+  Entity *ti_example;
+  YWidgetState *wid;
+
+  g_assert(!ygInitGameConfig(&cfg, NULL, SDL2));
+  g_assert(!ygInit(&cfg));
+  g_assert(ygLoadMod(TESTS_PATH"../modules/TextInput/"));
+  ti_example = ygFileToEnt(YJSON,
+			   TESTS_PATH"../modules/TextInput/input_test.json",
+				 NULL);
+  g_assert(ti_example);
+  wid = ywidNewWidget(ti_example, NULL);
+  g_assert(wid);
+  ywidSetMainWid(wid);
+  ygDoLoop();
+  yeDestroy(ti_example);
+  ygEnd();
+}
