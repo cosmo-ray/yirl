0c5e96f8ef4a9b8ea6de398dcd7528545b7f8beb
matthias <uso.cosmo.ray@gmail.com>
2017-12-07 10:25:19 +0100

    yirl-loader: remove debug
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- core/generic-loader/main.c
+++ core/generic-loader/main.c
@@ -78,21 +78,16 @@ int main(int argc, char **argv)
     start_need_free = 0;
   }
 
-  printf("not init yet\n");
   ygInitGameConfig(&cfg, start, render);
-  printf("cfg init\n");
   cfg.win_name = name;
   if (width > 0)
     cfg.w = width;
   if (height > 0)
     cfg.h = height;
-  printf("init\n");
   if (ygInit(&cfg) < 0)
     goto end;
-  printf("init done start loop\n");
   if (ygStartLoop(&cfg) < 0)
     goto end;
-  printf("running\n");
   ret = 0;
  end:
   ygEnd();
