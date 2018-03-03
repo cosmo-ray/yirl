146690916f48b019c7883f2fac79e2688da84812
matthias <uso.cosmo.ray@gmail.com>
2018-03-03 14:01:30 +0100

    package: use LD_LIBRARY_PATh instead of packaging ld-linux, as it doesn't work with opengl and stuff
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- package-game.sh
+++ package-game.sh
@@ -3,6 +3,7 @@ rm -rvf $2
 cp -rvf $1 $2
 cp -rvf ./include $2/
 cp -rvf ./modules $2/
+cp -rvf ./scripts-dependancies/ $2/
 cp -rvf tinycc/libtcc1.a $2/
 export LD_LIBRARY_PATH=./
 ./package-maker.sh ./yirl-loader $2/yirl-loader
--- package-maker.sh
+++ package-maker.sh
@@ -2,9 +2,8 @@
 NAME=$( basename $1 )
 mkdir $2
 cp $( ldd $1 | grep '=>' | cut -d ' ' -f 3 ) $2/
-cp $( ldd /bin/bash | tail -n 1 | cut -d ' ' -f 1 ) $2/
 echo '#!/bin/bash' >> $2/$NAME.sh
 echo 'dir=$( dirname "$0" )' >> $2/$NAME.sh
-echo "\$dir/ld-linux-x86-64.so.2 --library-path \$dir/ \$dir/$NAME \$@" >> $2/$NAME.sh
+echo "LD_LIBRARY_PATH=\$dir \$dir/$NAME \$@" >> $2/$NAME.sh
 chmod +x $2/$NAME.sh
 cp $1 $2/
