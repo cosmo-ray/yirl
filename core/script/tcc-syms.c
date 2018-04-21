d43e31fdff59ae38851fd102b9f80e0a7e37af90
U-BUFF-CLAN\matthias <uso.cosmo.ray@gmail.com>
2018-04-21 22:40:30 +0200

    tcc: indentation
    Signed-off-by: U-BUFF-CLAN\matthias <uso.cosmo.ray@gmail.com>

--- core/script/tcc-syms.c
+++ core/script/tcc-syms.c
@@ -32,39 +32,39 @@ void	fflushout(void)
 void	tccAddSyms(TCCState *l)
 {
 #ifdef WIN32
-    tcc_add_symbol(l, "free", free);
-    tcc_add_symbol(l, "fflushout", fflushout);
-    tcc_add_symbol(l, "strdup", strdup);
-    tcc_add_symbol(l, "printf", printf);
-    tcc_add_symbol(l, "ygFileToEnt", ygFileToEnt);
-    tcc_add_symbol(l, "yeGetByStrFast", yeGetByStrFast);
-    tcc_add_symbol(l, "yBlockArrayIteratorCreate", yBlockArrayIteratorCreate);
-    tcc_add_symbol(l, "memmove", memmove);
-    tcc_add_symbol(l, "yBlockArrayIteratorIncr", yBlockArrayIteratorIncr);
-    tcc_add_symbol(l, "yeGetString", yeGetString);
-    tcc_add_symbol(l, "strlen", strlen);
-    tcc_add_symbol(l, "snprintf", snprintf);
-    tcc_add_symbol(l, "yeRenamePtrStr", yeRenamePtrStr);
-    tcc_add_symbol(l, "ywTextureNewImg", ywTextureNewImg);
-    tcc_add_symbol(l, "ywRectCreateInts", ywRectCreateInts);
-    tcc_add_symbol(l, "yeCreateArrayByCStr", yeCreateArrayByCStr);
-    tcc_add_symbol(l, "yeGetInt", yeGetInt);
-    tcc_add_symbol(l, "ywCanvasNewImgFromTexture", ywCanvasNewImgFromTexture);
-    tcc_add_symbol(l, "ywCanvasRotate", ywCanvasRotate);
-    tcc_add_symbol(l, "yeLen", yeLen);
-    tcc_add_symbol(l, "yeStrCmp", yeStrCmp);
-    tcc_add_symbol(l, "yeCreateString", yeCreateString);
-    tcc_add_symbol(l, "yeCreateInt", yeCreateInt);
-    tcc_add_symbol(l, "yeCreateFloat", yeCreateFloat);
-    tcc_add_symbol(l, "yeDestroy", yeDestroy);
-    tcc_add_symbol(l, "yePushBack", yePushBack);
-    tcc_add_symbol(l, "ygGetManager", ygGetManager);
-    tcc_add_symbol(l, "yeCreateFunction", yeCreateFunction);
-    tcc_add_symbol(l, "yeGetFloat", yeGetFloat);
-    tcc_add_symbol(l, "yeGetByIdx", yeGetByIdx);
-    tcc_add_symbol(l, "yeSetInt", yeSetInt);
-    tcc_add_symbol(l, "yesCallInt", yesCallInt);
+  tcc_add_symbol(l, "free", free);
+  tcc_add_symbol(l, "fflushout", fflushout);
+  tcc_add_symbol(l, "strdup", strdup);
+  tcc_add_symbol(l, "printf", printf);
+  tcc_add_symbol(l, "ygFileToEnt", ygFileToEnt);
+  tcc_add_symbol(l, "yeGetByStrFast", yeGetByStrFast);
+  tcc_add_symbol(l, "yBlockArrayIteratorCreate", yBlockArrayIteratorCreate);
+  tcc_add_symbol(l, "memmove", memmove);
+  tcc_add_symbol(l, "yBlockArrayIteratorIncr", yBlockArrayIteratorIncr);
+  tcc_add_symbol(l, "yeGetString", yeGetString);
+  tcc_add_symbol(l, "strlen", strlen);
+  tcc_add_symbol(l, "snprintf", snprintf);
+  tcc_add_symbol(l, "yeRenamePtrStr", yeRenamePtrStr);
+  tcc_add_symbol(l, "ywTextureNewImg", ywTextureNewImg);
+  tcc_add_symbol(l, "ywRectCreateInts", ywRectCreateInts);
+  tcc_add_symbol(l, "yeCreateArrayByCStr", yeCreateArrayByCStr);
+  tcc_add_symbol(l, "yeGetInt", yeGetInt);
+  tcc_add_symbol(l, "ywCanvasNewImgFromTexture", ywCanvasNewImgFromTexture);
+  tcc_add_symbol(l, "ywCanvasRotate", ywCanvasRotate);
+  tcc_add_symbol(l, "yeLen", yeLen);
+  tcc_add_symbol(l, "yeStrCmp", yeStrCmp);
+  tcc_add_symbol(l, "yeCreateString", yeCreateString);
+  tcc_add_symbol(l, "yeCreateInt", yeCreateInt);
+  tcc_add_symbol(l, "yeCreateFloat", yeCreateFloat);
+  tcc_add_symbol(l, "yeDestroy", yeDestroy);
+  tcc_add_symbol(l, "yePushBack", yePushBack);
+  tcc_add_symbol(l, "ygGetManager", ygGetManager);
+  tcc_add_symbol(l, "yeCreateFunction", yeCreateFunction);
+  tcc_add_symbol(l, "yeGetFloat", yeGetFloat);
+  tcc_add_symbol(l, "yeGetByIdx", yeGetByIdx);
+  tcc_add_symbol(l, "yeSetInt", yeSetInt);
+  tcc_add_symbol(l, "yesCallInt", yesCallInt);
 #else
-    (void)l;
+  (void)l;
 #endif
 }
