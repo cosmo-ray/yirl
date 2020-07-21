19d2ece94a578f516c88de0931e3f80d33a11ec7
matthias <uso.cosmo.ray@gmail.com>
2020-07-21 23:15:04 +0200

    includes: add all.h
    Useful for tinycc.
    
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

---
+++ include/yirl/all.h
@@ -0,0 +1,26 @@
+/*
+**Copyright (C) 2020 Matthias Gatto
+**
+**This program is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Lesser General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+**
+**This program is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**GNU General Public License for more details.
+**
+**You should have received a copy of the GNU Lesser General Public License
+**along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <yirl/entity.h>
+#include <yirl/events.h>
+#include <yirl/entity-script.h>
+#include <yirl/map.h>
+#include <yirl/game.h>
+#include <yirl/menu.h>
+#include <yirl/container.h>
+#include <yirl/canvas.h>
+#include <yirl/text-screen.h>
