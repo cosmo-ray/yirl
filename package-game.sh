eafad83aba78660a56668accdc4e85ab0297bcf8
matthias <uso.cosmo.ray@gmail.com>
2020-11-08 02:13:51 +0100

    build: add docker support to pakcage-game
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- package-game.sh
+++ package-game.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
+BUILD_DST=.
+
+if [ $# -eq 3 ]; then
+	BUILD_DST=$3
+elif [ $# -ne 2 ]; then
+    echo "Usage: package-game.sh src dst [docker-build]"
+    return 1
+fi
+
 rm -rvf $2
 cp -rvf $1 $2
 cp -rvf ./include $2/
 cp -rvf ./modules $2/
 cp -rvf ./scripts-dependancies/ $2/
 cp -v ./DejaVuSansMono.ttf $2/
-install -D tinycc/libtcc1.a $2/tinycc/libtcc1.a
+install -D $BUILD_DST/tinycc/libtcc1.a $2/tinycc/libtcc1.a
 
-./package-maker.sh ./yirl-loader $2/yirl-loader
+if [ $# -eq 2 ]; then
+    ./package-maker.sh ./yirl-loader $2/yirl-loader
+else
+    cp -rvf $BUILD_DST $2/yirl-loader
+fi
 DIR_NAME=$( basename $2 )
 EXEC_NAME=$DIR_NAME\_start.sh
 echo yirl-loader/yirl-loader.sh -d ./ -n $DIR_NAME > $2/$EXEC_NAME
