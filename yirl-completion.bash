272a08cfeb15161af5ced26a8688e343d8fe13d7
Matthias Gatto <matthias.gatto@protonmail.com>
2022-05-14 01:45:40 +0200

    yirl-completion.bash: directory help
    Signed-off-by: Matthias Gatto <matthias.gatto@protonmail.com>

--- yirl-completion.bash
+++ yirl-completion.bash
@@ -1,1 +1,15 @@
-complete -W "--name --width --height --arg --linux-user-path --binary-root-path --start-dir" yirl-loader
+_yirl()
+{
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    if [ "--arg" == $prev -o "--height" == $prev -o "--name" == $prev -o "--width" == $prev ]; then
+	return
+    elif [ "--binary-root-path" == $prev -o "--start-dir" ==  $prev ]; then
+	COMPREPLY=($(compgen -o  dirnames -- "${cur}"))
+	return
+    fi
+    COMPREPLY=($(compgen -W "--name --width --height --arg --linux-user-path --binary-root-path --start-dir" -- "${cur}"))
+}
+
+complete  -F _yirl yirl-loader
