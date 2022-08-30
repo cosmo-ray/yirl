5ac60091b8dd1129bb382fde77bfab54c67e3f85
Matthias Gatto <matthias.gatto@protonmail.com>
2022-08-30 17:50:32 +0200

    Update utils.h

--- include/yirl/utils.h
+++ include/yirl/utils.h
@@ -52,6 +52,21 @@ static inline char* stpcpy(char *dest, const char *src)
 	return dest;
 }
 
+static char *strndup(char *str, int chars)
+{
+    char *buffer;
+    int n;
+
+    buffer = (char *) malloc(chars +1);
+    if (buffer)
+    {
+        for (n = 0; ((n < chars) && (str[n] != 0)) ; n++) buffer[n] = str[n];
+        buffer[n] = 0;
+    }
+
+    return buffer;
+}
+
 #endif
 
 #endif
