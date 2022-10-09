3c329975ff4a5f9ae664021e40631d3f39cb3316
Matthias Gatto <uso.cosmo.ray@gmail.com>
2022-10-09 18:59:53 +0200

    pkg: remove gl and x libs
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- package-maker.sh
+++ package-maker.sh
@@ -3,8 +3,8 @@ NAME=$( basename $1 )
 mkdir $2
 ls -l
 export LD_LIBRARY_PATH=./
-ldd $1 | grep '=>' | cut -d ' ' -f 3
-cp -v $( ldd $1 | grep '=>' | cut -d ' ' -f 3 | grep -v  -e libm.so -e libc.so -e libstdc++.so -e libdl.so.2 -e ld-linux-x86-64.so.2 -e libpthread.so.0 -e librt.so) $2/
+#ldd $1 | grep '=>' | cut -d ' ' -f 3
+cp -v $( ldd $1 | grep '=>' | cut -d ' ' -f 3 | grep -v  -e libm.so  -e libc.so -e libstdc++.so -e libdl.so.2 -e ld-linux-x86-64.so.2 -e libpthread.so.0 -e librt.so -e lib*GL* -e libXau -e libxcb -e libX11 -e libXext -e libOpenGL.so -e linux-vdso -e libXdmcp ) $2/
 echo '#!/bin/bash' >> $2/$NAME.sh
 echo 'dir=$( dirname "$0" )' >> $2/$NAME.sh
 echo "LD_LIBRARY_PATH=\$dir \$dir/$NAME \$@" >> $2/$NAME.sh
