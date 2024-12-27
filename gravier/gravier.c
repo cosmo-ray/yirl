a90d61e81c763e98270075ddbacee7602ef414b2
Matthias Gatto <uso.cosmo.ray@gmail.com>
2024-12-27 16:13:35 +0100

    add gravier binary source
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

---
+++ gravier/gravier.c
@@ -0,0 +1,13 @@
+#define GRAVIER_ENABLE_DEBUG
+
+#include "perl.h"
+
+int main(int ac, char **av)
+{
+	PerlInterpreter *perl = perl_alloc();
+	perl_construct(perl);
+	perl_parse(perl, NULL, ac, av, NULL);
+	perl_run(perl);
+	perl_destruct(perl);
+	perl_free(perl);
+}
