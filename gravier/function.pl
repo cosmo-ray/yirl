c1eabeb1ad78d753232e5a17a02d89fd4844bb07
Matthias Gatto <uso.cosmo.ray@gmail.com>
2025-01-07 04:15:28 +0100

    gravier: function test
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

---
+++ gravier/function.pl
@@ -0,0 +1,33 @@
+
+my $oui = "oui";
+
+sub oui
+{
+    if ($oui eq "skip") {
+	return;
+    }
+    print $oui;
+}
+
+oui;
+$oui = "non";
+oui();
+print("\n");
+$oui = "skip";
+oui;
+
+#sub print_01_nl
+#{
+#    sub print_01 {
+#	print $_[1];
+#    }
+#    sub print_01 {
+#	print $_[0];
+#    }
+#    print_01 $_[0];
+#    print_01("\n");
+#}
+
+#print_01 "oh";
+
+#print_01_nl "ah";
