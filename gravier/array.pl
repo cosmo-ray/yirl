bb286ee0687c16a41fb4d210e89e9368cfa5fa8a
Matthias Gatto <uso.cosmo.ray@gmail.com>
2025-01-17 20:09:23 +0100

    gravier: add array test
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

---
+++ gravier/array.pl
@@ -0,0 +1,19 @@
+
+$ar2[1] = "tata";
+$ar2[0] = "titi";
+
+print $ar2[0], "\n";
+print $ar2[1], "\n";
+
+@ar = (25, 30, "out");
+
+print $ar[0], "\n";
+print $ar[1], "\n";
+print $ar[2], "\n";
+
+print $ar[$none], "\n";
+$ar[$none] = 7;
+print $ar[$none], "\n";
+
+$one=1;
+print $ar[$one], "\n";
