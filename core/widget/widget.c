55c0e7a5370600975cdfa68bf8781f53a89ce1ec
Matthias Gatto <uso.cosmo.ray@gmail.com>
2026-05-26 02:50:19 +0200

    widgets: add 'red', 'blue', 'green', as colors
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- core/widget/widget.c
+++ core/widget/widget.c
@@ -328,6 +328,24 @@ int ywidColorFromString(const char *str, uint8_t *r, uint8_t *g,
 		*b = c & 0x000000ff;
 		*a = 0xff;
 		ret = 0;
+	} else if (!strcmp(str, "red")) {
+		*r = 255;
+		*g = 0;
+		*b = 0;
+		*a = 255;
+		ret = 0;
+	} else if (!strcmp(str, "blue")) {
+		*r = 0;
+		*g = 0;
+		*b = 255;
+		*a = 255;
+		ret = 0;
+	} else if (!strcmp(str, "green")) {
+		*r = 0;
+		*g = 255;
+		*b = 0;
+		*a = 255;
+		ret = 0;
 	} else if (!strcmp(str, "white")) {
 		*r = 255;
 		*g = 255;
